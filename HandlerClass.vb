31c02c147f6964d0193b76dfe0b7ee8745cfb323
gekim360 <gekim360@gmail.com>
2021-05-28 22:10:04 -0400

    File  to  control status after updating config

---
+++ HandlerClass.vb
@@ -0,0 +1,24 @@
+﻿Imports System.ServiceProcess
+
+Public Class HandlerClass
+    Public Shared Sub ServiceAction()
+        Dim controller As New ServiceController("Solunix.WebServer")
+        Dim controller2 As New ServiceController("SolunixWebServerResetConfig")
+        Try
+            controller.Stop()
+            controller.WaitForStatus(ServiceControllerStatus.Stopped)
+            controller.Start()
+            controller.WaitForStatus(ServiceControllerStatus.Running)
+            controller2.Stop()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Shared Sub Terminate()
+        Try
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
---
+++ ServiceRestart.vb
@@ -0,0 +1,12 @@
+﻿Public Class ServiceRestart
+
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        HandlerClass.ServiceAction()
+    End Sub
+
+    Protected Overrides Sub OnStop()
+
+    End Sub
+
+End Class
+
